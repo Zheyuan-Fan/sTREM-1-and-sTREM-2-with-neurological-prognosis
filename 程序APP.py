fa5a18c7c57c30a161727b50c04e1189a751bc8d
Zheyuan-Fan <zheyuan94fan@gmail.com>
2025-04-30 21:54:34 +0800

    程序APP.py

--- 程序APP.py
+++ 程序APP.py
@@ -57,7 +57,7 @@ if st.button("Predict"):
     probability = predicted_proba[predicted_class] * 100
 
     # 显示预测结果，使用 Matplotlib 渲染指定字体
-    text = f"Based on feature values, predicted possibility of Non-survivors is {probability:.2f}%"
+    text = f"Based on feature values, predicted possibility of poor neurological outcome is {probability:.2f}%"
     fig, ax = plt.subplots(figsize=(8, 1))
     ax.text(
         0.5, 0.5, text,
